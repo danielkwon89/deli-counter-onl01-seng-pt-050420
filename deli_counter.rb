361f7c1cc866cc98a8d9768b0c2d6ade9d21d377
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 20:41:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def now_serving(array)
   if array.length == 0
     puts "There is nobody waiting to be served!"
   elsif array.length > 0
-    
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
