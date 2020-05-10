9778b26af25744b3aac62b22c7e2188aa0f1daaf
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 20:32:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
     name_array = ["The line is currently:"]
     counter = 1
     array.each do |name|
-      name_array.push(" #{counter}. #{name} ")
+      name_array.push(" #{counter}. #{name}")
       counter += 1
     end #each iterator
     name_string = name_array.join
