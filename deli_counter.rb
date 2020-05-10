19b5859d90b9734141cb5904fe42eb7b48c813f0
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 20:15:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  end #if statement
+  else if array.length > 0
+    counter = 1
+    array.each do |name|
+      puts "#{counter}. #{name} "
+      counter += 1
+    end #each iterator
+  end #else if statement
+end #line method
