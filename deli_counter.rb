5cab8d5dff9f1fb8c52f343becff298204e85c70
Daniel Kwon <danielkwon89@gmail.com>
2020-05-10 20:22:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,11 @@
 def line(array)
   if array.length == 0
     puts "The line is currently empty."
-  else if array.length > 0
+  elsif array.length > 0
     counter = 1
     array.each do |name|
       puts "#{counter}. #{name} "
       counter += 1
     end #each iterator
   end #if statement
-  end
 end #line method
